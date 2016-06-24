8ebda571e820fa832dd46c4818dc7628fc1418a7
catorrep <p25castro@gmail.com>
2016-06-24 18:46:31 +0200

    SFE5dim using the variables Bayer, Deutsche Telekom and Volkswagen of the datafile "2004-2014_dax_ftse.csv"

---
+++ SFE5dim/SFE5dim.R
@@ -0,0 +1,55 @@
+# clear variables and close windows
+rm(list = ls(all = TRUE))
+graphics.off()
+
+# set working directory
+# setwd("C:/...")
+setwd("Z:/SFE5dim")
+
+# load libraries
+# install and load packages
+libraries = c("data.table", "fGarch")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+# load data
+dataset = fread("2004-2014_dax_ftse.csv", select =  c( "BAYER", "DEUTSCHE TELEKOM", "VOLKSWAGEN"))
+dataset = as.data.frame(dataset)
+
+# the entries of this data frame will be replaced by the log-returns
+X = dataset[-1,]
+
+# number of observations
+obs = nrow(dataset)
+
+# log-returns
+for (i in seq_len(ncol(dataset))) {
+  X[,i] = log(dataset[-1,i]/dataset[-obs,i])
+}
+
+# the entries of this data frame will be replaced by the epsilon values from the fitted Garch model
+eps = X
+
+for (i in seq_len(ncol(dataset))) {
+  fit      = garchFit(~garch(1, 1), data = X[, i], trace = F)
+  eps[, i] = fit@residuals / fit@sigma.t
+}
+
+f.eps = eps
+
+for(i in seq_len(ncol(dataset))) {
+  # making margins uniform, based on Ranks
+  f.eps[,i] = rank(eps[,i]) / obs
+}
+
+panel.cor <- function(x, y, pch)
+{
+  par(usr = c(-0.01, 1.01, -0.01, 1.01))
+  x = rank(x) / (length(x) + 1)
+  y = rank(y) / (length(y) + 1)
+  points(x, y, pch = pch)
+}
+
+pairs(eps, lower.panel = panel.cor, pch = 20)
