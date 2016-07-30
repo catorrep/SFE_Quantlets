052c8ede748ee8fede99997dd9645606a5d9fff1
catorrep <p25castro@gmail.com>
2016-07-30 22:34:23 +0200

    SFEexrate with the USD to Euro hourly exchange rate

--- SFEexrate/SFEexrate.m
+++ SFEexrate/SFEexrate.m
@@ -7,36 +7,38 @@ close all
 %cd('C:/...')
 
 %% data import
-formatSpec = '%{dd/MM/yyyy}D%f'
-data       = readtable('2004_2014_exRate_EURUSD.csv','Delimiter',',', 'Format',formatSpec);
+formatSpec = '%{dd/MM/yyyy}D%f%f';
+data       = readtable('2010_2016_exRate_USDEUR_HR.csv','Delimiter',',', 'Format',formatSpec);
+data       = sortrows(data, {'Date','Hour'});
 
 %% Convert FX-rates to return series
-returns = diff(log(data.EUDOLLR)); 
+returns = diff(log(data.USDEUR)); 
 
 %% These variables will be later used for the label of the time axis
 date_Year       = year(data.Date);
-where_put_Rate  = [true; (diff(date_Year) == 1)];
+where_put_Rate  = [false; (diff(date_Year) == 1)];
 Years           = date_Year(where_put_Rate);
-where_put_Rtrn  = [true; (diff(date_Year(2:end)) == 1)];           
+where_put_Rtrn  = [false; (diff(date_Year(2:end)) == 1)];           
 Index_Rate      = 1:size(data,1);
 Index_Rtrn      = 1:(size(data,1)-1);
 
-%% Open the window for the plots
+
+%% Open a new window for the plots
 figure
 set(gcf,'color','w') % set the background color to white
 
-%% Plot FX EUR/USD
+%% Plot FX USD/EUR
 subplot(2, 1, 1) 
-plot(Index_Rate, data.EUDOLLR)
-  title('FX Rate EUR/USD')
-    axis([0 Index_Rate(end) min(data.EUDOLLR) max(data.EUDOLLR)])
+plot(Index_Rate, data.USDEUR)
+  title('FX Rate USD/EUR')
+    axis([0 Index_Rate(end) min(data.USDEUR) max(data.USDEUR)])
     set(gca, 'XTick', Index_Rate(where_put_Rate))
     set(gca,'XTickLabels', Years)
 
-%% Plot returns EUR/USD    
+%% Plot returns USD/EUR
 subplot(2,1,2) 
 plot(Index_Rtrn, returns)
-  title('FX Returns EUR/USD')
+  title('FX Returns USD/EUR')
     axis([0  Index_Rtrn(end) min(returns) max(returns)])
     set(gca,'XTick', Index_Rtrn(where_put_Rtrn))
     set(gca,'XTickLabels', Years)
